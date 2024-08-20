6328d205d3641cc77cfc4ae3c7e33fb5bf0e3df1
Nitesh Salian <niteshsalian8652085233@gmail.com>
2024-08-20 21:03:35 +0530

    added moving cursor

--- script.js
+++ script.js
@@ -1,6 +1,38 @@
+ crsr=document.querySelector('.cursor')
+document.addEventListener("mousemove",function(dets){
+    // console.log(dets)
+    crsr.style.left=dets.clientX+"px"
+    crsr.style.top=dets.clientY+"px"
+})
+
 gsap.to("#nav",{
     backgroundColor:"#000",
     duration:1,
- 
-
+height:"80px" ,
+scrollTrigger:{
+    trigger:"#nav",
+    scroller:"body",
+    // markers:true,
+    start:"top -15%",
+    end:"top -16%",
+    scrub:1
+}
 })
+
+gsap.to("#main",{
+backgroundColor:"#000",
+scrollTrigger:{
+    trigger:"#main",
+    scroller:"body",
+    start:"top -15%",
+    end:"top -15%",
+    scrub:1
+}
+})
+
+
+
+
+
+
+
